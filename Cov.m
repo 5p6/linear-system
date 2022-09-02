34aed9a7b89c43aecb8241dd370bed35396cd48e
handsome boy <100855117+5p6@users.noreply.github.com>
2022-09-02 11:11:18 +0800

    Update Cov.m

--- Cov.m
+++ Cov.m
@@ -1,6 +1,8 @@
 function f=Cov(X)
+%求协方差矩阵
 %X为装载着行向量数据的矩阵
 %X为M×n
+%求均值后再转置
 m=mean(X)';
 TX=X';
 [x,y]=size(TX);
@@ -8,6 +10,7 @@ Cov=zeros(x,x);
 for i = 1:y
     Cov=Cov+(TX(:,i)-m)*((TX(:,i)-m)');
 end
+%求均值
 Cov=Cov./(y-1);
 f=Cov;
 end
